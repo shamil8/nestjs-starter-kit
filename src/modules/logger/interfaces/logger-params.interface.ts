affcaa0d999f1cf23a3ef311285efbd8ff603584
shamil8 <qurbonovshamil@gmail.com>
2022-09-12 01:20:56 +0700

    Changed winston config

--- src/modules/logger/interfaces/logger-params.interface.ts
+++ src/modules/logger/interfaces/logger-params.interface.ts
@@ -2,7 +2,7 @@ import { WinstonDefaultLogLevel } from '../enums/winston-default-log-level';
 
 export interface LoggerParamsInterface {
   level: WinstonDefaultLogLevel;
-  isRotateLoggerFilesActivated: boolean;
+  isRotateLoggerFile: boolean;
   loggerName: string;
   loggerMaxSize: string;
   loggerMaxFile: string;
